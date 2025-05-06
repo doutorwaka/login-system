b356b840a4da66c6bf27e3d6f9867e887463d333
José Eurípedes <jeferreirajf@gmail.com>
2025-05-06 10:41:20 -0300

    feat: add refresh token usecase

---
+++ backend/src/usecases/user/refresh-auth-token/refresh-auth-token-user.usecase.ts
@@ -0,0 +1,45 @@
+import { UserGateway } from 'src/domain/repositories/user.gateway';
+import { JwtService } from 'src/infra/services/jwt/jwt.service';
+import { CredentialsNotValidUsecaseException } from 'src/usecases/exceptions/credentials-not-valid.usecase.exception';
+import { Usecase } from 'src/usecases/usecase';
+
+export type RefreshAuthTokenUserUsecaseInput = {
+  refreshToken: string;
+};
+
+export type RefreshAuthTokenUserUsecaseOutput = {
+  authToken: string;
+};
+
+export class RefreshAuthTokenUserUsecase
+  implements
+    Usecase<RefreshAuthTokenUserUsecaseInput, RefreshAuthTokenUserUsecaseOutput>
+{
+  public constructor(
+    private readonly userGateway: UserGateway,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  public async execute({
+    refreshToken,
+  }: RefreshAuthTokenUserUsecaseInput): Promise<RefreshAuthTokenUserUsecaseOutput> {
+    const { authToken, userId } =
+      await this.jwtService.generateAuthTokenWithRefreshToken(refreshToken);
+
+    const anUser = await this.userGateway.findById(userId);
+
+    if (!anUser) {
+      throw new CredentialsNotValidUsecaseException(
+        `User with id ${userId} not found while refreshing auth token with refresh token ${refreshToken} in ${RefreshAuthTokenUserUsecase.name}`,
+        `Credenciais inválidas`,
+        RefreshAuthTokenUserUsecase.name,
+      );
+    }
+
+    const output: RefreshAuthTokenUserUsecaseOutput = {
+      authToken,
+    };
+
+    return output;
+  }
+}
