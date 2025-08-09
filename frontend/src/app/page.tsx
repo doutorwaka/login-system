390f20eaf4fbf71401cf2809a02844f81087999a
José Eurípedes <jeferreirajf@gmail.com>
2025-08-09 12:30:09 -0300

    feature: add shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <>Olá Dr. Waka!</>;
+    return (
+        <div>
+            <p>Olá Dr. Waka</p>
+            <div>
+                <Button>Entrar</Button>
+            </div>
+        </div>
+    );
 }
