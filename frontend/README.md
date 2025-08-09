8c719d7c17171a9b1706f2442e2ece6c17b12c35
José Eurípedes <jeferreirajf@gmail.com>
2025-08-09 12:19:42 -0300

    feature: start frontend app

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Login System Frontend App
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Sistema de Login do Dr. Waka",
+    description: "Sistema de login gerado na aula de login do Dr Waka",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="pt-BR">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
