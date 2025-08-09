9089fb63d931e2d64ac2b60869ac237adc110afd
José Eurípedes <jeferreirajf@gmail.com>
2025-08-09 12:57:13 -0300

    feature: add login card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { LoginCard } from "@/components/login/login-card";
 
 export default function Home() {
     return (
-        <div>
-            <p>Olá Dr. Waka</p>
-            <div>
-                <Button>Entrar</Button>
-            </div>
+        <div className="w-screen h-screen flex items-center justify-center bg-foreground/5">
+            <LoginCard />
         </div>
     );
 }
